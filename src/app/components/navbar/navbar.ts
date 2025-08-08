7028c26c19f377ac42200bebe7c532701d5b07a2
Alan Pustkowiak <alan.pustkowiak@outlook.com>
2025-08-08 22:28:25 +0200

    basic navbar

---
+++ src/app/components/navbar/navbar.ts
@@ -0,0 +1,11 @@
+import { Component, input } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  imports: [],
+  templateUrl: './navbar.html',
+  styleUrl: './navbar.scss'
+})
+export class Navbar {
+  title = input<string>('KudosCore');
+}
