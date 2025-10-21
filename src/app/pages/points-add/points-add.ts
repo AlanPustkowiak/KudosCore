cd00d4d794d2ba1aea50c97f420f3b2875f00506
Alan Pustkowiak <alan.pustkowiak@outlook.com>
2025-10-21 20:17:17 +0200

    basic adding form completed

--- src/app/pages/points-add/points-add.ts
+++ src/app/pages/points-add/points-add.ts
@@ -38,7 +38,7 @@ export class PointsAdd implements OnInit {
     this.pointsForm = this.formBuilder.group({
       employeeId: ['', Validators.required],
       points: ['', [Validators.required, Validators.min(1)]],
-      reason: ['', Validators.required],
+      comment: ['', Validators.required],
     });
   }
 
@@ -51,19 +51,24 @@ export class PointsAdd implements OnInit {
     if (this.pointsForm.valid) {
       this.isSubmitting.set(true);
       const formValue = this.pointsForm.value;
+
       const selectedEmployee = this.employees$().find(
-        (emp) => emp.id === formValue.employeeId
+        (emp) => emp.id === +formValue.employeeId
       );
 
       const newKudos = {
         to: selectedEmployee ? selectedEmployee.name : 'Unknown',
-        from: 'System', // In a real app, this would be the logged-in user
-        comment: formValue.reason,
+        from: 'System',
+        comment: formValue.comment,
         points: formValue.points,
       };
       this.kudosService.addKudos(newKudos);
       this.isSubmitting.set(false);
       this.router.navigate(['/points-list']);
     }
+  }
+
+  onCancel(): void {
+    this.router.navigate(['/points-list']);
   }
 }
